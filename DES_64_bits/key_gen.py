a9e351b438cb21246b111d32e9fa70274d20f334
vilasini-v <gulkavarsh@gmail.com>
2025-01-26 17:09:32 +0530

    64 bits des

--- DES_64_bits/key_gen.py
+++ DES_64_bits/key_gen.py
@@ -1,4 +1,5 @@
 # This file contains the functions to generate the subkeys from the given key
+import data
 def left_circular_shift(arr, x):
     if(x==1):
         a = arr[0]
@@ -27,10 +28,6 @@ def generate_key_halves(k2, x):
     r=left_circular_shift(r, x)
     return l,r
 
-p10= [3,5,2,7,4,10,1,9,8,6]
-p56=[57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
-pc48 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
-left_shifts = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
     
 def generateKey(c, d, pc48):
     k=[]
@@ -40,6 +37,8 @@ def generateKey(c, d, pc48):
     return k
 
 def generateAllKeys(k1):
+    #retrieve data from data.py
+    p10, p56, pc48, left_shifts = data.key_data()
     c0,d0 = permute_56(k1, p56)
     print('Key 0: ', c0+d0)
     c=[]
